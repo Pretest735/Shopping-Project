2bbf630410b2d5780d8d0101e782c927eb1566b5
Pretest735 <mdiqbalhafiz735@gmail.com>
2018-08-10 17:17:31 +0600

    Implement update and delete item

--- shopping.go
+++ shopping.go
@@ -6,6 +6,7 @@ import(
 	"net/http"
 	"github.com/gorilla/mux"
 	"encoding/json"
+	"strconv"
 )
 
 
@@ -37,9 +38,48 @@ func addItems(w http.ResponseWriter,r *http.Request){
 
 }
 func updateItem(w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
+	in := mux.Vars(r)
+	for idx, tmp := range items{
+		var cnv string
+		cnv = strconv.Itoa(tmp.ID)
+		if(in["id"] == cnv){
+			items = append(items[:idx] , items[idx + 1:]...)
+			w.Header().Set("Content-Type","application/json")
+			var tmp2 Items
+			_ = json.NewDecoder(r.Body).Decode(&tmp2)
+			tmp2.ID = tmp.ID
+			if(tmp2.Name == "") {
+				tmp2.Name = tmp.Name
+			}
+			if(tmp2.Price == 0){
+				tmp2.Price = tmp.Price;
+			}
+			if(tmp2.Quantity == 0){
+				tmp2.Quantity = tmp.Quantity
+			}
+			items = append(items,tmp2)
+			json.NewEncoder(w).Encode(tmp2)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(items)
+	
 	
 }
 func deleteItem(w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","application/json")
+	in := mux.Vars(r)
+	for idx, tmp := range items{
+		var cnv string
+		cnv = strconv.Itoa(tmp.ID)
+		if(in["id"] == cnv){
+			items = append(items[:idx] , items[idx + 1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(items)
+
 	
 }
 
@@ -54,10 +94,10 @@ func main() {
 	items = append(items,Items{ID: 2 , Name : "Pant" , Price: 1250 , Quantity: 2})
 
 	mx.HandleFunc("/",HomePage)
-	mx.HandleFunc("/api/items",showItems).Methods("GET")
-	mx.HandleFunc("/api/items",addItems).Methods("POST")
-	mx.HandleFunc("/api/items/{id}",updateItem).Methods("PUT")
-	mx.HandleFunc("/api/items/{id}",deleteItem).Methods("DELETE")
+	mx.HandleFunc("/shop/items",showItems).Methods("GET")
+	mx.HandleFunc("/shop/items",addItems).Methods("POST")
+	mx.HandleFunc("/shop/items/{id}",updateItem).Methods("PUT")
+	mx.HandleFunc("/shop/items/{id}",deleteItem).Methods("DELETE")
 
 	http.ListenAndServe(":8080",mx)
 }
